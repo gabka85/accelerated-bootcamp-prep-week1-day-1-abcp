98fd821d809fef56968466b6ed53ae64ac9934e2
Gabrielle Dimarsky <gabbydimarsky14@gmail.com>
2018-01-13 18:18:25 +0000

    automatically backed up by learn

--- scope.js
+++ scope.js
@@ -18,6 +18,7 @@ function setSpeciesVariable() {
 function catInfo() {
   name = "Felix"
   var mood = "sleepy"
+  species = "fish"
   console.log("Species is " + species);
   console.log("Name is " + name);
   console.log("Mood is " + mood)
